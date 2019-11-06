c31f9c4996a3e940e103ab487d80aef1f3a68352
danbarretto <daniel_barretto98@hotmail.com>
2019-11-06 16:26:55 -0300

    Add line connectors

--- src/components/Exercises/Ex2/Ex2.js
+++ src/components/Exercises/Ex2/Ex2.js
@@ -1,14 +1,171 @@
-import React from 'react'
-import '../../../App.css'
+import React, { useEffect } from "react";
+import "../../../App.css";
+import Organells from "../../json/Organells";
+import LeftBox from "./LeftBox";
+import Grid from "@material-ui/core/Grid/Grid";
+import RightBox from "./RightBox";
+import { jsPlumb } from "jsplumb";
 
-export default function Ex2(props){
+export default class Ex2 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      first: null,
+      lines: [],
+      hasLines: false
+    };
+  }
 
-    return(
-        <div>
-            
-            <p className='titulo'>Associe as Organelas</p>
-            <p>Associe as organelas com suas respectivas funções celulares:</p>
+  componentDidMount() {
+    jsPlumb.ready(function() {
+      var els = document.querySelectorAll(".wrapper");
 
-        </div>
-    )
+      var target = {
+        isSource: false,
+        isTarget: true,
+        connector: "Straight",
+        endpoint: "Rectangle",
+        paintStyle: { fill: "white", outlineStroke: "blue", strokeWidth: 3 },
+        hoverPaintStyle: { outlineStroke: "lightblue" },
+        connectorStyle: { outlineStroke: "green", strokeWidth: 1, margin:'15px' },
+        connectorHoverStyle: { strokeWidth: 2 },
+        margin:15
+      };
+      var source = {
+        isSource: true,
+        isTarget: false,
+        connector: "Straight",
+        endpoint: "Rectangle",
+        paintStyle: { fill: "white", outlineStroke: "blue", strokeWidth: 3 },
+        hoverPaintStyle: { outlineStroke: "lightblue" },
+        connectorStyle: { outlineStroke: "green", strokeWidth: 1, margin:'15px' },
+        connectorHoverStyle: { strokeWidth: 2 },
+        margin:15
+      };
+
+
+
+      jsPlumb.draggable("A");
+      jsPlumb.addEndpoint(
+          "A",
+          { anchors: ["Left"] },
+          target
+        );
+      
+      jsPlumb.draggable("B");
+
+      jsPlumb.addEndpoint(
+          "B",
+          { anchors: ["Left"] },
+          target
+        );
+      jsPlumb.draggable("C");
+
+      jsPlumb.addEndpoint(
+          "C",
+          { anchors: ["Left"] },
+          target
+        );
+      jsPlumb.draggable("D");
+
+      jsPlumb.addEndpoint(
+          "D",
+          { anchors: ["Left"] },
+          target
+        );
+
+      jsPlumb.draggable("Núcleo")
+      jsPlumb.addEndpoint(
+          "Núcleo",
+          {anchors:["Right"]},
+          source
+      )
+      jsPlumb.draggable("Retículo Endoplasmático Rugoso");
+      jsPlumb.addEndpoint(
+        "Retículo Endoplasmático Rugoso",
+        { anchors: ["Right"] },
+        source
+      );
+      jsPlumb.draggable("Mitocôndria");
+      jsPlumb.addEndpoint("Mitocôndria", { anchors: ["Right"] }, source);
+
+      jsPlumb.draggable("Lisossomo");
+      jsPlumb.addEndpoint("Lisossomo", { anchors: ["Right"] }, source);
+
+
+    });
+  }
+
+
+  render() {
+    const left = [];
+
+    const desc = [
+      <RightBox
+        id="A"
+        className="Retículo Endoplasmático Rugoso"
+        desc="Modificação de proteínas"
+      />,
+      <RightBox
+        id="B"
+        className="Núcleo"
+        desc="Duplicação do DNA e produção de RNA"
+      />,
+      <RightBox
+        id="C"
+        className="Mitocôndria"
+        desc="Produção de energia"
+      />,
+      <RightBox
+        id="D"
+        className="Lisossomo"
+        desc="Digestão intracelular"
+      />
+    ];
+    let i = 0;
+    Organells.organells.forEach(element => {
+      if (
+        element.name === "Núcleo" ||
+        element.name === "Mitocôndria" ||
+        element.name === "Lisossomo" ||
+        element.name === "Retículo Endoplasmático Rugoso"
+      ) {
+        left.push(
+          <Grid
+            container
+            direction="row"
+            justify="space-between"
+            alignItems="center"
+          >
+            <LeftBox
+              id={element.name}
+              handleClick={e => {
+                this.setState({ first: e.target });
+              }}
+              
+              img={element.gif}
+              desc={element.name}
+            />
+            {desc[i]}
+          </Grid>
+        );
+        i++;
+      }
+    });
+
+    return (
+      <>
+        <Grid
+          container
+          direction="column"
+          justify="space-between"
+          alignItems="baseline"
+        >
+          <p className="titulo">Associe as Organelas</p>
+          <p>Associe as organelas com suas respectivas funções celulares:</p>
+          {left}
+        </Grid>
+      </>
+    );
+  }
 }
