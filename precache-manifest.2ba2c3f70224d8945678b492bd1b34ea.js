c61c4f3cab52849ae2269f2daaa514db50103799
danbarretto <daniel_barretto98@hotmail.com>
2020-02-04 10:45:09 -0300

    Updates

---
+++ precache-manifest.2ba2c3f70224d8945678b492bd1b34ea.js
@@ -0,0 +1,238 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c63f001d850fc39307c8fe590efdcbcc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c73c8452022c870b8262",
+    "url": "/static/css/main.22ce4516.chunk.css"
+  },
+  {
+    "revision": "3300fd05d608158f288f",
+    "url": "/static/js/2.e14ef7a3.chunk.js"
+  },
+  {
+    "revision": "414ce368446ed2edabb883697ddad177",
+    "url": "/static/js/2.e14ef7a3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c73c8452022c870b8262",
+    "url": "/static/js/main.fa68fbe1.chunk.js"
+  },
+  {
+    "revision": "25a2c844bd730a130ed8",
+    "url": "/static/js/runtime-main.1c348993.js"
+  },
+  {
+    "revision": "9379dfd5910570e79e79bce2963ab6ff",
+    "url": "/static/media/Hip Hop Dancing.9379dfd5.glb"
+  },
+  {
+    "revision": "7cfb3239a66ac671bd7edd5a6571124b",
+    "url": "/static/media/Zombie Stand Up.7cfb3239.dae"
+  },
+  {
+    "revision": "ccb02deb9d1cf051c3283d3969cb81bb",
+    "url": "/static/media/bacteria.ccb02deb.svg"
+  },
+  {
+    "revision": "12de36d01f9086473c943788e2ed385f",
+    "url": "/static/media/bg.12de36d0.svg"
+  },
+  {
+    "revision": "5eec3a8b7b12fbc3de0d9a16330bca7e",
+    "url": "/static/media/capsula.5eec3a8b.mp3"
+  },
+  {
+    "revision": "2c580c29f9618a77a4d942a78b983750",
+    "url": "/static/media/celulaAnimal.2c580c29.svg"
+  },
+  {
+    "revision": "2a0acc46f39fda5f6fbaf70019244b49",
+    "url": "/static/media/celulaProcarionte.2a0acc46.svg"
+  },
+  {
+    "revision": "01302c20ad73c8cf90c0f2e707f4ad08",
+    "url": "/static/media/celulaVegetal.01302c20.svg"
+  },
+  {
+    "revision": "85d42575c679d52c43666c8618127b6e",
+    "url": "/static/media/centriolos.85d42575.mp3"
+  },
+  {
+    "revision": "e02d30f09b4340a6394b36ac0d3ae095",
+    "url": "/static/media/centriolos.e02d30f0.gif"
+  },
+  {
+    "revision": "bc10e55a03d76a673553a12b8d40fab5",
+    "url": "/static/media/cibfar.bc10e55a.svg"
+  },
+  {
+    "revision": "31505547911ace8bf45cca39aa55c4fa",
+    "url": "/static/media/citoesqueleto.31505547.gif"
+  },
+  {
+    "revision": "65a3bd2c1136b0260396f7854c4ef125",
+    "url": "/static/media/citoesqueleto.65a3bd2c.mp3"
+  },
+  {
+    "revision": "aa43ac7d36709dace42467a32a420b9f",
+    "url": "/static/media/cloroplasto.aa43ac7d.mp3"
+  },
+  {
+    "revision": "ea7f4d32cc75c07c4b050126abb3bc11",
+    "url": "/static/media/cloroplasto.ea7f4d32.gif"
+  },
+  {
+    "revision": "f9ea149714770ac86d1889e8de8ed858",
+    "url": "/static/media/eic.f9ea1497.svg"
+  },
+  {
+    "revision": "351b893529e5b805aa56e5f01e3ffec9",
+    "url": "/static/media/ex1.351b8935.svg"
+  },
+  {
+    "revision": "875538e94694cbb10026f1d2288252df",
+    "url": "/static/media/fapesp.875538e9.svg"
+  },
+  {
+    "revision": "1dd8997b7602c5119b7e39207320c4ae",
+    "url": "/static/media/fimbria.1dd8997b.mp3"
+  },
+  {
+    "revision": "f473f786d70408e0d0f743f609cd0c5e",
+    "url": "/static/media/flagelo.f473f786.mp3"
+  },
+  {
+    "revision": "52689475a16616f3cc7f137329aa1489",
+    "url": "/static/media/glioxissomo.52689475.gif"
+  },
+  {
+    "revision": "b386e6a3ebae961761926a168867348b",
+    "url": "/static/media/glioxissomo.b386e6a3.mp3"
+  },
+  {
+    "revision": "6e8ccd42215ce3faa9c0e13bcc8febf7",
+    "url": "/static/media/golgi.6e8ccd42.gif"
+  },
+  {
+    "revision": "9aeef95284e2137db81e27f2f692c8c3",
+    "url": "/static/media/golgi.9aeef952.mp3"
+  },
+  {
+    "revision": "74213bbc9982701c1b0cfbd164a2f395",
+    "url": "/static/media/lisossomo.74213bbc.gif"
+  },
+  {
+    "revision": "7b769b6fb3fd49b7fdebe37d0907e8ac",
+    "url": "/static/media/lisossomo.7b769b6f.mp3"
+  },
+  {
+    "revision": "fb58dd3629fc75c8fa883eefee03259c",
+    "url": "/static/media/logo.fb58dd36.svg"
+  },
+  {
+    "revision": "32d2023de7543467cf1a5902c25109c5",
+    "url": "/static/media/membrana.32d2023d.gif"
+  },
+  {
+    "revision": "c2bdf8a369209ba641d938c0fedf1ab8",
+    "url": "/static/media/membrana.c2bdf8a3.mp3"
+  },
+  {
+    "revision": "d305b7fde3d4c676defa4da90262c2f0",
+    "url": "/static/media/mesossomo.d305b7fd.mp3"
+  },
+  {
+    "revision": "7a9e91b903fb50a620e0e8977890832f",
+    "url": "/static/media/mitocondria.7a9e91b9.mp3"
+  },
+  {
+    "revision": "e54cfa84121f3e96e25309b005157e23",
+    "url": "/static/media/mitocondria.e54cfa84.gif"
+  },
+  {
+    "revision": "9688957f3f2ddca9ab70edf48e4e474b",
+    "url": "/static/media/nucleo.9688957f.gif"
+  },
+  {
+    "revision": "9cbd9345be3cdafd5d9b925158e5c4bb",
+    "url": "/static/media/nucleo.9cbd9345.mp3"
+  },
+  {
+    "revision": "ca30f679963867212f05ac45606a9683",
+    "url": "/static/media/nucleoide.ca30f679.mp3"
+  },
+  {
+    "revision": "a799b695a3ad3833a3585f226a838d06",
+    "url": "/static/media/parede_pro.a799b695.mp3"
+  },
+  {
+    "revision": "1dcde2010ae1ba5858b6ab0e1c5f1c2e",
+    "url": "/static/media/parede_veg.1dcde201.mp3"
+  },
+  {
+    "revision": "c52ddd8d834e24082ca71b3ea1ff490b",
+    "url": "/static/media/peroxissomo.c52ddd8d.mp3"
+  },
+  {
+    "revision": "cc9977f9e5db2d74e6cee9c1bc80039b",
+    "url": "/static/media/peroxissomo.cc9977f9.gif"
+  },
+  {
+    "revision": "c91cf5d8d80805a6a830113fb3a42d53",
+    "url": "/static/media/plasmideo.c91cf5d8.mp3"
+  },
+  {
+    "revision": "325b79f98459f2a53e5824181a69987c",
+    "url": "/static/media/plasmodesmo.325b79f9.mp3"
+  },
+  {
+    "revision": "21673f000faedb992418d3544150db67",
+    "url": "/static/media/reliso.21673f00.gif"
+  },
+  {
+    "revision": "96624efd481386ea141afc430b8d1388",
+    "url": "/static/media/reliso.96624efd.mp3"
+  },
+  {
+    "revision": "566d7d8c0b67e40d6203294ca120e2e4",
+    "url": "/static/media/rerugoso.566d7d8c.mp3"
+  },
+  {
+    "revision": "84787be65a68760333813155ceb0f165",
+    "url": "/static/media/rerugoso.84787be6.gif"
+  },
+  {
+    "revision": "35ac0c12e723f0b2c312dd106ae07fa2",
+    "url": "/static/media/ribossomo.35ac0c12.mp3"
+  },
+  {
+    "revision": "f0bde61d4186fa2618652e7e08784206",
+    "url": "/static/media/ribossomo.f0bde61d.gif"
+  },
+  {
+    "revision": "4d7961a331f703080b3f9f13cda19dae",
+    "url": "/static/media/test.4d7961a3.svg"
+  },
+  {
+    "revision": "59b7a4ed40287454873bfe108bb9eded",
+    "url": "/static/media/test_model.59b7a4ed.3mf"
+  },
+  {
+    "revision": "864faf39ac43b6670ce3f335f1aafa05",
+    "url": "/static/media/test_model.864faf39.stl"
+  },
+  {
+    "revision": "74b877140204acc2375dbe623c56fd1a",
+    "url": "/static/media/teste.74b87714.svg"
+  },
+  {
+    "revision": "85b07433c6740bef821d72348050e9ce",
+    "url": "/static/media/vacuolo.85b07433.gif"
+  },
+  {
+    "revision": "f619a13c8af3cc6d9c224cebbcfdf8fb",
+    "url": "/static/media/vacuolo.f619a13c.mp3"
+  }
+]);
