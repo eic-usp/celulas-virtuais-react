403e9f7b2885e7636d5984d09622cc5412d0e3f9
danbarretto <daniel_barretto98@hotmail.com>
2020-03-27 17:14:48 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/celulas-virtuais-react/precache-manifest.407e1ed87285089fa08335c148f8faad.js"
+  "/celulas-virtuais-react/precache-manifest.b201213f72c44946318624bfb9b0acb8.js"
 );
 
 self.addEventListener('message', (event) => {
